602eecee62d57c7cc2f115231c468e4caf6fb805
Ahmet Kaan Guney <68081622+AhmetKaanGuney@users.noreply.github.com>
2021-10-27 13:42:12 +0300

    Changed db structure and querying is working now.

--- src/app.py
+++ src/app.py
@@ -1,20 +1,21 @@
-#! "./venv/Scripts/python.exe"
+#!../venv/Scripts/python.exe
 
+import json
 import logging
-import flask
 import sqlite3
 
+import flask
+
 from file_io import XlsFile
-import json
 from matrix import Matrix, col_to_num, matrix_coordinates
 
 # ENCODING = "UTF-8"
-    # logger = logging.getLogger(__name__)
-    # logger.setLevel(logging.INFO)
-    # formatter = logging.Formatter("%(levelname)s:%(message)s")
-    # file_handler = logging.FileHandler("../logs/flask_test.log", mode="a", encoding=ENCODING)
-    # file_handler.setFormatter(formatter)
-    # logger.addHandler(file_handler)
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+# formatter = logging.Formatter("%(levelname)s:%(message)s")
+# file_handler = logging.FileHandler("../logs/flask_test.log", mode="a", encoding=ENCODING)
+# file_handler.setFormatter(formatter)
+# logger.addHandler(file_handler)
 
 # How this program works :
 # user sends file to server
@@ -33,32 +34,32 @@ from matrix import Matrix, col_to_num, matrix_coordinates
 
 # get source file and matrix coordinates from client
 coordinates = matrix_coordinates((2, 6), ("b", "f"))
-source_file = "./test/input_files/5-5.xls"
-user_id = 0
+source_file = "../test/input_files/5-5.xls"
+user_id = 3
 # check file type
 # convert file to Matrix() object    
 source_matrix = XlsFile(source_file, coordinates).parse()
-print(source_matrix.name, "\n", 
-  source_matrix.get_rows(),"\n", source_matrix.get_cols())
+print(source_matrix.name, "\n", source_matrix.get_rows(),"\n", source_matrix.get_cols())
 
 # convert Matrix() object to json format
-rows = ""
-cols = ""
-for name in source_matrix.get_rows():
-    rows += name + ","
-for name in source_matrix.get_cols():
-    cols += name + ","
-matrix_as_json = json.dumps({"rows": rows, "cols": cols})
+rows = json.dumps(source_matrix.get_rows())
+cols = json.dumps(source_matrix.get_cols())
+matrix_as_json = json.dumps({"rows": rows, "cols": cols}, indent=4)
 print(matrix_as_json)
 
 # store Matrix() in db with an id specific to user
-con = sqlite3.connect("./db/matricies.db")
-cur = con.cursor()
-cur.execute("INSERT INTO matricies VALUES (user_id=?, matrix=?)", 
-                                        user_id, matrix_as_json)
+conn = sqlite3.connect("../db/matricies.db")
+cur = conn.cursor()
+# cur.execute("INSERT INTO matricies (user_id, matrix) VALUES (?, ?)", 
+#                                         (user_id, matrix_as_json))
+# conn.commit()
 
-print(cur.excute("SELECT * FROM matricies"))
+print("ADDED to db: ")
+for row in cur.execute("SELECT matrix FROM matricies WHERE user_id=?", (user_id,)):
+    print(row)
 
+cur.close()
+conn.close()
 # return blueprint json to client
 
 # Second is:
